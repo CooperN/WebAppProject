8f2591e71be68c086753feced47188300702f3ce
neilcallen <neil.allen@okstate.edu>
2017-11-07 17:28:44 -0600

    LogIn
    Moved over code from SQL assignment for login, basically almost started over for login

--- PrereqApp/app/src/main/java/com/example/msis4363/pre_reqapp/Login.java
+++ PrereqApp/app/src/main/java/com/example/msis4363/pre_reqapp/Login.java
@@ -1,76 +1,119 @@
 package com.example.msis4363.pre_reqapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Login extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    // Declaring connection variables
+    public Connection con;
+    String un,pass,db,ip;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+        // End Getting values from button, texts and progress bar
+
+        // Declaring Server ip, username, database name and password
+        ip = "huckleberries.database.windows.net:1433";
+        db = "PrereqDB";
+        un = "hberryadmin";
+        pass = "Equifax1";
+        // Declaring Server ip, username, database name and password
+
+        // Setting up the function when button login is clicked
+        run.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                CheckLogin checkLogin = new CheckLogin();
+                checkLogin.execute("");
+            }
+        });
     }
 
-    //Executes code for login button click
-    public void onLogin(View v){
-
-        //Save login information
-        EditText fieldUserName = (EditText) findViewById(R.id.userName);
-        EditText fieldUserPass = (EditText) findViewById(R.id.userPass);
-
-        String userName = fieldUserName.getText().toString().toLowerCase();
-        String password = fieldUserPass.getText().toString();
+    public class CheckLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
+        String name1 = "";
 
 
-
-
-    }
-
-    public class LogIn extends AppCompatActivity {
-
-        Database helper = new Database(this);
+        protected void onPreExecute()
+        {
+            progressBar.setVisibility(View.VISIBLE);
+        }
 
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_login);
+        protected void onPostExecute(String r)
+        {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, r, Toast.LENGTH_LONG).show();
+            if(isSuccess)
+            {
+                message = (TextView) findViewById(R.id.textView2);
+                message.setText(name1);
+
+            }
         }
+        @Override
+        protected String doInBackground(String... params)
+        {
 
-        public void onLoginPressed (View v) {
-            EditText a = (EditText) findViewById(R.id.userName);
-            String userstr = a.getText().toString();
-            EditText b = (EditText) findViewById(R.id.userPass);
-            String passstr = b.getText().toString();
+            try
+            {
+                con = connectionclass();        // Connect to database
+                if (con == null)
+                {
+                    z = "Check Your Internet Access!";
+                }
+                else
+                {
+                    // Change below query according to your own database.
+                    String query = "select * from NeilAllen";
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery(query);
+                    if(rs.next())
+                    {
+                        name1= rs.getString("LName"); //Name is the string label of a column in database, read through the select query
+                        z = "query successful";
+                        isSuccess=true;
+                        con.close();
 
+                    }
+                    else
+                    {
+                        z = "Invalid Query!";
+                        isSuccess = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                isSuccess = false;
+                z = ex.getMessage();
 
-            String pass = helper.searchPass(userstr);
-            //created name strings so that the helper can pull from the DB
-            String firstnamestr = helper.searchFirstName(userstr);
-            String lastnamestr = helper.searchLastName(userstr);
-
-            if(passstr.equals(pass)){
-                Intent intent2 = new Intent(getApplicationContext(), Profile.class);
-                //added the name strings into the welcome message that gets passed on to the Profile page
-                intent2.putExtra("FLString", "Welcome " + firstnamestr + " " + lastnamestr + " " + "to your profile");
-                startActivity(intent2);
-
-
-            } else {
-                Toast toast = Toast.makeText(LogIn.this, "Password does not match", Toast.LENGTH_SHORT);
-                toast.show();
+                Log.d ("sql error", z);
             }
 
-
-        }
-
-        public void onRegisterPressed(View v){
-            Intent intent3 = new Intent(getApplicationContext(), Register.class);
-            startActivity(intent3);
+            return z;
         }
     }
-
-
-
 }
