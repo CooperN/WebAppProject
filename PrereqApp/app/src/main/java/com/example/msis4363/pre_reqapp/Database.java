01864b288d7783ca43ccb902dd56c071836d6c8f
neilcallen <neil.allen@okstate.edu>
2017-11-07 16:31:05 -0600

    Added to last DB Committ
    Tried to delete from last commit all the SQL lite methods

--- PrereqApp/app/src/main/java/com/example/msis4363/pre_reqapp/Database.java
+++ PrereqApp/app/src/main/java/com/example/msis4363/pre_reqapp/Database.java
@@ -28,29 +28,14 @@ import java.sql.Statement;
  */
 
 
-public class Database extends SQLiteOpenHelper {
 public class Database extends AppCompatActivity {
 
-    private static final int DATABASE_VERSION = 2;
-    private static final String DATABASE_NAME = "contacts.db";
-    private static final String TABLE_NAME = "contacts";
-    private static final String COLUMN_ID = "id";
-    private static final String COLUMN_NAME = "username";
-    private static final String COLUMN_FIRSTNAME = "firstname";
-    private static final String COLUMN_LASTNAME = "lastname";
-    private static final String COLUMN_PASSWORD = "password";
 
-    SQLiteDatabase db;
     // Declaring connection variables
     public Connection con;
     String un,pass,db,ip;
 
-    private static final String TABLE_CREATE = "create table contacts (id integer primary key not null, username text not null, password text not null, firstname not null, lastname not null);";
 
-    //this is the constructor method
-    public Helper (Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -68,38 +53,8 @@ public class Database extends AppCompatActivity {
         // Declaring Server ip, username, database name and password
 
 
-    //insert query method
-    public void insertContact (User c) {
-        db = this.getWritableDatabase();
-        //store values from different fields
-        ContentValues values = new ContentValues();
-
-        //auto increment id
-        String query = "Select * from contacts";
-        Cursor cursor = db.rawQuery(query, null);
-        int count = cursor.getCount();
-
-        values.put(COLUMN_ID, count);
-        values.put(COLUMN_NAME, c.getUsername());
-        values.put(COLUMN_PASSWORD, c.getPassword());
-        values.put(COLUMN_FIRSTNAME, c.getFirstName());
-        values.put(COLUMN_LASTNAME, c.getLastName());
-
-        db.insert(TABLE_NAME, null, values);
-        db.close();
     }
 
-    //method for looking up the password - basic structure of a select query method
-    public String searchPass (String username) {
-        db = this.getReadableDatabase();
-        String query = "select username, password from"+TABLE_NAME;
-        Cursor cursor = db.rawQuery(query, null);
-        String a,b;
-        b = "not found";
-        if (cursor.moveToFirst()){
-            do{
-                a = cursor.getString(0);
-                if (a.equals(username))
     public class CheckLogin extends AsyncTask<String,String,String>
     {
         String z = "";
@@ -148,39 +103,7 @@ public class Database extends AppCompatActivity {
 
             return z;
         }
-    public Contact getContact(String username) {
-        Contact c = new Contact();
-        db = this.getReadableDatabase();
-        String query = "select username, password, firstname, lastname from"+TABLE_NAME + "where username = "+username;
-        Cursor cursor = db.rawQuery(query, null);
-        String u,p,f,l;
-        cursor.moveToFirst();
-        u = cursor.getString(0);
-        p = cursor.getString(1);
-        f = cursor.getString(2);
-        l = cursor.getString(3);
-
-        c.setUsername(u);
-        c.setPassword(p);
-        c.setFirstName(f);
-        c.setLastName(l);
-
-        return c;
     }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(TABLE_CREATE);
-        this.db = db;
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        //drop query
-        String query = "DROP TABLE IF EXISTS "+TABLE_NAME;
-        sqLiteDatabase.execSQL(query);
-        this.onCreate(sqLiteDatabase);
 
     public Connection connectionclass()
     {
