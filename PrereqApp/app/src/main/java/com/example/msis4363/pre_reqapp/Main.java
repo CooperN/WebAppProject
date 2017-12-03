2de99962d9a3f7b69d5e078fe472dc8d5b111e88
CooperN <norman.cooper.j@gmail.com>
2017-12-03 11:45:07 -0600

    Funcitoning MainMenu page

--- PrereqApp/app/src/main/java/com/example/msis4363/pre_reqapp/Main.java
+++ PrereqApp/app/src/main/java/com/example/msis4363/pre_reqapp/Main.java
@@ -1,7 +1,10 @@
 package com.example.msis4363.pre_reqapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -10,4 +13,24 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btnClassesToTake(View v) {
+        Intent intent = new Intent(getApplicationContext(), ClassesToTake.class);
+        startActivity(intent);
+
+    }
+
+    public void btnFinishedCourses(View v) {
+        Intent intent2 = new Intent(getApplicationContext(), FinishedCourses.class);
+        startActivity(intent2);
+
+    }
+
+    public void btnDegreeSheet(View v) {
+        String url = "https://business.okstate.edu/site-files/docs/msis/2016-2017-MIS.pdf";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
 }
